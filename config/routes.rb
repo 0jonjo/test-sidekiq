34dc6e17b18f28bc674ce04408b18ab72164e4da
jonjo <joaogilberto@tuta.io>
2021-10-17 21:45:47 -0300

    add views, routes and controllers defs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  root 'contacts#index' 
+  root 'contacts#home' 
 
-  get '/contacts', to: 'contacts#index'
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
